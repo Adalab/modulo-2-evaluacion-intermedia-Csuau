0b3e00f011ac002ed901eef22c8306fed3873e4b
csuau <csuaubou@gmail.com>
2022-07-06 10:20:25 +0200

    add const and fuctions  js

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
 'use strict';
+const number = document.querySelector('.js_number');
+const btn = document.querySelector('.js_btn');
+const clue = document.querySelector('.js_clue');
+const attemps = document.querySelector('.js_attemps');
+let countClick = 0;
+function getRandomNumber (max) {
+    return Math.ceil(Math.random() * max);
+}
+
+const RandomNumber = getRandomNumber(100);
+console.log(RandomNumber);
+
+function handleClick (ev) {
+    ev.preventDefault();
+    const numberValue = number.value;
+    countClick += 1;
+    attemps.innerHTML = countClick;
+
+    if (numberValue == RandomNumber) {
+        clue.innerHTML = 'Has ganado campeona!!!';
+    } else if (numberValue > RandomNumber && numberValue < 100) {
+        clue.innerHTML = 'Demasiado alto.';
+    } else if (numberValue < RandomNumber && numberValue > 0) {
+        clue.innerHTML = 'Demasiado bajo.';
+    } else {
+        clue.innerHTML = 'El número debe estar entre 1 y 100.';
+    }
+}
+
+
+btn.addEventListener('click', handleClick);
+//console.log()
