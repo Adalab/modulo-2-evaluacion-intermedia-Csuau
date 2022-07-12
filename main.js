d700669332a9c852e69aefa977c9098149f28e3e
csuau <csuaubou@gmail.com>
2022-07-12 19:16:28 +0200

    Resolt issues JS,Structure and HTML

--- main.js
+++ main.js
@@ -4,28 +4,41 @@ const btn = document.querySelector('.js_btn');
 const clue = document.querySelector('.js_clue');
 const attemps = document.querySelector('.js_attemps');
 let countClick = 0;
+
+
 function getRandomNumber (max) {
     return Math.ceil(Math.random() * max);
 }
+const randomNumber = getRandomNumber(100);
+console.log(randomNumber);
 
-const RandomNumber = getRandomNumber(100);
-console.log(RandomNumber);
+function checkNumber () {
+    const numberValue = parseInt(number.value);
 
-function handleClick (ev) {
-    ev.preventDefault();
-    const numberValue = number.value;
-    countClick += 1;
-    attemps.innerHTML = countClick;
-
-    if (numberValue == RandomNumber) {
+    if (isNaN(numberValue)) {
+        clue.innerHTML = 'El número debe estar entre 1 y 100.';
+    }
+    if (numberValue === randomNumber) {
         clue.innerHTML = 'Has ganado campeona!!!';
-    } else if (numberValue > RandomNumber && numberValue < 100) {
+    } else if (numberValue > randomNumber && numberValue < 100) {
         clue.innerHTML = 'Demasiado alto.';
-    } else if (numberValue < RandomNumber && numberValue > 0) {
+    } else if (numberValue < randomNumber && numberValue > 0) {
         clue.innerHTML = 'Demasiado bajo.';
     } else {
         clue.innerHTML = 'El número debe estar entre 1 y 100.';
     }
+}
+
+function incrementCounter () {
+    countClick++;
+    attemps.innerHTML = countClick;
+}
+
+function handleClick (ev) {
+    ev.preventDefault();
+    checkNumber();
+    count();
+
 }
 
 
